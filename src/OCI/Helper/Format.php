634ae2b5560db2955da5c1d447932e7cdb4e1cc8
Elie NEHME <elie29@gmail.com>
2019-04-10 11:52:48 +0200

    ✨ Refactory SessionInit and provide PHP format #37

---
+++ src/OCI/Helper/Format.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace OCI\Helper;
+
+interface Format
+{
+    /**
+     * oracle NLS session format
+     */
+    public const NLS_TIME = 'HH24:MI:SS';
+    public const NLS_DATE = 'YYYY-MM-DD HH24:MI:SS';
+    public const NLS_TIMESTAMP = 'YYYY-MM-DD HH24:MI:SS';
+    public const NLS_TIMESTAMP_TZ = 'YYYY-MM-DD HH24:MI:SS TZH:TZM';
+
+    /**
+     * php date format equivalent to oracle session format
+     */
+    public const PHP_TIME = 'H:i:s';
+    public const PHP_DATE = 'Y-m-d H:i:s';
+    public const PHP_TIMESTAMP = 'Y-m-d H:i:s';
+    public const PHP_TIMESTAMP_TZ = 'Y-m-d H:i:s P';
+}
--- src/OCI/Helper/SessionInit.php
+++ src/OCI/Helper/SessionInit.php
@@ -8,11 +8,7 @@ use OCI\Driver\DriverInterface;
 
 /**
  * The following environment variables are required for default date format:
- *
- * NLS_TIME_FORMAT='HH24:MI:SS'
- * NLS_DATE_FORMAT='YYYY-MM-DD HH24:MI:SS'
- * NLS_TIMESTAMP_FORMAT='YYYY-MM-DD HH24:MI:SS'
- * NLS_TIMESTAMP_TZ_FORMAT='YYYY-MM-DD HH24:MI:SS TZH:TZM'
+ * @see Format
  */
 class SessionInit
 {
@@ -21,10 +17,10 @@ class SessionInit
      * @var array
      */
     protected $defaultSessionVars = [
-        'NLS_TIME_FORMAT' => 'HH24:MI:SS',
-        'NLS_DATE_FORMAT' => 'YYYY-MM-DD HH24:MI:SS',
-        'NLS_TIMESTAMP_FORMAT' => 'YYYY-MM-DD HH24:MI:SS',
-        'NLS_TIMESTAMP_TZ_FORMAT' => 'YYYY-MM-DD HH24:MI:SS TZH:TZM',
+        'NLS_TIME_FORMAT' => Format::NLS_TIME,
+        'NLS_DATE_FORMAT' => Format::NLS_DATE,
+        'NLS_TIMESTAMP_FORMAT' => Format::NLS_TIMESTAMP,
+        'NLS_TIMESTAMP_TZ_FORMAT' => Format::NLS_TIMESTAMP_TZ,
         'NLS_NUMERIC_CHARACTERS' => '.,',
     ];
 
